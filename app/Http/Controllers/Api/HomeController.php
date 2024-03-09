f2f1bfc8ebfa4c8a8f40b2d9032d93372844395b
AhmedMossad1 <ahmedmosad525@gmail.com>
2024-03-09 14:38:24 +0200

    Add attendance in Home page

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -3,29 +3,36 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\MessageService;
 use App\Services\ProgramService;
+use App\Services\AttendanceService;
 
 class HomeController extends Controller
 {
     protected $programService;
     protected $messageService;
+    protected $attendanceService;
 
-    public function __construct(ProgramService $programService,MessageService $messageService)
+    public function __construct(ProgramService $programService,MessageService $messageService,AttendanceService $attendanceService)
     {
         $this->programService = $programService;
         $this->messageService = $messageService;
+        $this->attendanceService = $attendanceService;
     }
 
     public function index()
     {
         $user = auth()->user();
+        $userId = auth()->id();
         $programsPercentage = $this->programService->calculateProgramPercentage($user);
         $messagePercentage = $this->messageService->calculateMessagePercentage($user);
+        $productivity = max($programsPercentage,$messagePercentage);
+        $attendanceService = $this->attendanceService->calculatePointsForUser($userId);
+
 
         return response()->json([
             'data' => [
-                'name' => $user->name,
-                'Program percentage' => $programsPercentage,
-                'message percentage' => $messagePercentage,
+                'productivity percentage' => $productivity,
+                'attend percentage' => $attendanceService,
+
             ],
         ]);
     }
