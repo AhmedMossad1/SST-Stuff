95ac520edbc3ceb4d2d342354c4b9c28b913e3e7
AhmedMossad1 <ahmedmosad525@gmail.com>
2024-03-09 16:04:27 +0200

    Make Changes in Errors Section add function to calculate points in ErrorsService

--- app/Http/Controllers/Api/ErrorsController.php
+++ app/Http/Controllers/Api/ErrorsController.php
@@ -3,22 +3,29 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ErrorsResource;
 use App\Models\Error as ModelsError;
+use Carbon\Carbon;
+use App\Services\ErrorsService;
+
 class ErrorsController extends Controller
 {
-    public function index(){
-        $errors = ModelsError::where('user_id',auth()->id())->get();
-        $total = $errors->sum('degree');
-        if($total <= 100){
-            $percentage = (100-$total)/100;
-        }
-        else{
-            $percentage=0;
-        }
-        return response()->json([
-            'data' => [
-                'percentage'=>$percentage,
-                'errors' => ErrorsResource::collection($errors),
-            ],
-    ]);
+    protected $errorsService;
+
+    public function __construct(ErrorsService $errorsService)
+    {
+        $this->errorsService = $errorsService;
     }
+    public function index(){
+        {
+            $userId = auth()->id();
+            $percentage = $this->errorsService->calculateErrorsPercentage($userId);
+            $errors = $this->errorsService->getErrorsForCurrentMonth($userId);
+
+            return response()->json([
+                'data' => [
+                    'percentage' => $percentage,
+                    'errors' => ErrorsResource::collection($errors),
+                ],
+            ]);
+        }
+}
 }
